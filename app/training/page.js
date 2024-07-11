61a80f718d5ede27701270c35f1fe3662ecc44da
eptakam <emmataks@gmail.com>
2024-07-11 12:16:56 -0300

    Protecting routes againdt unauthenticated access.

--- app/training/page.js
+++ app/training/page.js
@@ -1,6 +1,16 @@
+import { verifyAuth } from '@/lib/auth';
 import { getTrainings } from '@/lib/training';
+import { redirect } from 'next/navigation';
 
 export default async function TrainingPage() {
+  // verifier si l'utilisateur est authentifie 
+  const result = await verifyAuth();
+
+  if (!result.user) {
+    // rediriger l'utilisateur vers la page de connexion s'il n'est pas authentifie
+    return redirect('/');
+  }
+
   const trainingSessions = getTrainings();
 
   return (
