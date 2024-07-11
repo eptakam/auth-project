ebe0bf86983ae33a7651e325bc8515fb7ad2a014
eptakam <emmataks@gmail.com>
2024-07-11 09:54:08 -0300

    Handling the errors due to the form.

--- components/auth-form.js
+++ components/auth-form.js
@@ -14,19 +14,40 @@ export default function AuthForm() {
       <p>
         <label htmlFor="email">Email</label>
         <input type="email" name="email" id="email" />
+        {/* afficher les erreurs dues a l'email en dessous du champ de saisie */}
+        {formState.errors &&
+        // Object.keys() : convertit l'objet passe en argument en tableau de clés
+          Object.keys(formState.errors)
+            .filter((error) => error === "email")
+            .map((error) => (
+              <div key={error} className="input-errors">
+                {formState.errors[error]}
+              </div>
+            ))}
       </p>
       <p>
         <label htmlFor="password">Password</label>
         <input type="password" name="password" id="password" />
+        {/* afficher les erreurs dues au password en dessous du champ de saisie */}
+        {formState.errors &&
+        // Object.keys : convertit l'objet passe en argument en tableau de clés
+          Object.keys(formState.errors)
+            .filter((error) => error === "password")
+            .map((error) => (
+              <div key={error} className="input-errors">
+                {formState.errors[error]}
+              </div>
+            ))}
       </p>
-      {formState.errors && (
-        <ul id="form-errors">
+      {/* afficher les erreurs generales au-dessus du bouton 'Create Account' */}
+      {/* {formState.errors && (
+        <ul id="form-errors"> */}
           {/* Object.keys : convertit un objet en tableau de clés */}
-          {Object.keys(formState.errors).map((error) => (
+          {/* {Object.keys(formState.errors).map((error) => (
             <li key={error}>{formState.errors[error]}</li>
           ))}
         </ul>
-      )}
+      )} */}
       <p>
         <button type="submit">Create Account</button>
       </p>
